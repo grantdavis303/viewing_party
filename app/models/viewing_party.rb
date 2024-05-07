1de30b64185b3ad77a2a17b18875937f04da8815
Grant Davis <grantdavis303@gmail.com>
2024-05-07 17:26:59 -0600

    Format Models

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,8 +1,8 @@
 class ViewingParty < ApplicationRecord
-   has_many :user_parties
-   has_many :users, through: :user_parties
+  has_many :user_parties
+  has_many :users, through: :user_parties
 
-   def find_host
-      users.where("user_parties.host = true").first
-   end
+  def find_host
+    users.where("user_parties.host = true").first
+  end
 end
