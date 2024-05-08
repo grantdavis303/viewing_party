d36e8d4f2bfccdb3acbae9206834f0d73ca1b987
Grant Davis <grantdavis303@gmail.com>
2024-05-07 20:01:26 -0600

    Add Spec Test for User Discover Movies Page

---
+++ spec/features/movies/discover_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User Discover Movies Page', type: :feature do
+
+  # User Story 1
+  it "has search button and movie discover button" do
+    # Setup
+    @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
+
+    # As a user, when I visit the '/users/:id/discover' path (where :id is the id of a valid user),
+    visit "/users/#{@user.id}/discover"
+
+    # I should see
+    within ".find_movies" do
+      # Button to Discover Top Rated Movies
+      expect(page).to have_link("Discover Top Rated Movies")
+      # Text field to enter keyword(s) to search by movie title
+      expect(page).to have_field(:search_by_movie_title)
+      # Button to Search by Movie Title
+      expect(page).to have_button("Search")
+    end
+  end
+
+end
