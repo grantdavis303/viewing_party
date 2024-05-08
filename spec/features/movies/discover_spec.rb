15088a658490c567efe64aa0a3bc51761a0ec003
Grant Davis <grantdavis303@gmail.com>
2024-05-07 20:12:47 -0600

    Update US1 Test

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -13,12 +13,16 @@ RSpec.describe 'User Discover Movies Page', type: :feature do
     # I should see
     within ".find_movies" do
       # Button to Discover Top Rated Movies
-      expect(page).to have_link("Discover Top Rated Movies")
+      expect(page).to have_link("Discover Top Rated Movies", href: "/users/#{@user.id}/movies")
       # Text field to enter keyword(s) to search by movie title
       expect(page).to have_field(:search_by_movie_title)
       # Button to Search by Movie Title
       expect(page).to have_button("Search")
     end
+
+    # Clicking the form Search button brings the user to the movies page
+    click_on ("Search")
+    expect(current_path).to eq("/users/#{@user.id}/movies")
   end
 
 end
