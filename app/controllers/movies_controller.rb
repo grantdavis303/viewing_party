a512c6931cde35baccccd63a5e77fa0cf23fe661
Grant Davis <grantdavis303@gmail.com>
2024-05-08 07:34:33 -0600

    Update Movies Controller with 1 Request

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,19 +16,13 @@ class MoviesController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #https://api.themoviedb.org/3/movie/550?api_key=0f7ff543b9146c27bb69c85b227e5f63&language=en-US&append_to_response=credits
 
     # Movie Information
     movie_id = params["movie_id"]
-    request = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=0f7ff543b9146c27bb69c85b227e5f63")
-    @requested_movie = JSON.parse(request.body)
-
-    # Movie Cast
-    cast_request = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=0f7ff543b9146c27bb69c85b227e5f63")
-    @requested_movie_cast = JSON.parse(cast_request.body)["cast"][0...10]
-
-    # Movie Reviews
-    review_request = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}/reviews?api_key=0f7ff543b9146c27bb69c85b227e5f63")
-    @requested_movie_reviews = JSON.parse(review_request.body)["results"]
+    request = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=0f7ff543b9146c27bb69c85b227e5f63&append_to_response=credits,reviews")
+    parsed_json = JSON.parse(request.body)
+    @requested_movie = parsed_json
+    @requested_movie_cast = parsed_json["credits"]["cast"][0...10]
+    @requested_movie_reviews = parsed_json["reviews"]["results"]
   end
 end
