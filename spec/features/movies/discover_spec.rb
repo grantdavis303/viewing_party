ad0621f35e33baf57cf19125855448832a251449
Grant Davis <grantdavis303@gmail.com>
2024-05-08 07:33:40 -0600

    Finish US3

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -26,22 +26,16 @@ RSpec.describe 'User Discover Movies Page', type: :feature do
     # Setup
     @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
     json_response = File.read('spec/fixtures/movies_list.json')
-    stub_request(:get, "https://api.themoviedb.org/3/trending/movie/week?api_key=0f7ff543b9146c27bb69c85b227e5f63")
-      .to_return(status: 200, body: json_response)
+    stub_request(:get, "https://api.themoviedb.org/3/trending/movie/week?api_key=0f7ff543b9146c27bb69c85b227e5f63").to_return(status: 200, body: json_response)
 
-    # When I visit the discover movies page ('/users/:id/discover'),
     visit "/users/#{@user.id}/discover"
 
-    # and click on either the Discover Top Rated Movies button,
     click_on ("Discover Top Rated Movies")
 
-    # I should be taken to the movies results page (`users/:user_id/movies`) where I see: 
     expect(current_path).to eq("/users/#{@user.id}/movies")
 
     within ".movies_list" do
-      # Title (As a Link to the Movie Details page (see story #3))
       expect(page).to have_link("Dune: Part Two", href: "/users/#{@user.id}/movies/693134")
-      # Vote Average of the movie      
       expect(page).to have_content("Vote Average: 8.2")
       expect(page).to have_link("Godzilla Minus One", href: "/users/#{@user.id}/movies/940721")
       expect(page).to have_content("Vote Average: 7.8")
@@ -55,27 +49,20 @@ RSpec.describe 'User Discover Movies Page', type: :feature do
 
   # User Story 2 - Path 2 (Search)
   it "search button leads to the movies page" do
-
     # Setup
     @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
     json_response = File.read('spec/fixtures/movies_list_tron.json')
-    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=0f7ff543b9146c27bb69c85b227e5f63&query=Tron")
-      .to_return(status: 200, body: json_response)
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=0f7ff543b9146c27bb69c85b227e5f63&query=Tron").to_return(status: 200, body: json_response)
 
-    # When I visit the discover movies page ('/users/:id/discover'),
     visit "/users/#{@user.id}/discover"
 
-    # and fill out the movie title search and click the Search button,
     fill_in :search_by_movie_title, with: "Tron"
     click_on ("Search")
 
-    # I should be taken to the movies results page (`users/:user_id/movies`) where I see: 
     expect(current_path).to eq("/users/#{@user.id}/movies")
   
     within ".movies_list" do
-      # Title (As a Link to the Movie Details page (see story #3))
       expect(page).to have_link("Tron", href: "/users/#{@user.id}/movies/97")
-      # Vote Average of the movie      
       expect(page).to have_content("Vote Average: 6.7")
       expect(page).to have_link("Throne", href: "/users/#{@user.id}/movies/1231003")
       expect(page).to have_content("Vote Average: 0.0")
@@ -83,7 +70,6 @@ RSpec.describe 'User Discover Movies Page', type: :feature do
       expect(page).to have_content("Vote Average: 6.47")
     end
     
-    # I should also see a button to return to the Discover Page.
     expect(page).to have_link("Return to Discover Page", href: "/users/#{@user.id}/discover")
   end
 end
--- spec/features/movies/movies_spec.rb
+++ spec/features/movies/movies_spec.rb
@@ -7,30 +7,49 @@ RSpec.describe 'Movies Show Page', type: :feature do
     # Setup
     @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
     json_response = File.read('spec/fixtures/movie_dune.json')
-    stub_request(:get, "https://api.themoviedb.org/3/movie/693134?api_key=0f7ff543b9146c27bb69c85b227e5f63")
-      .to_return(status: 200, body: json_response)
+    stub_request(:get, "https://api.themoviedb.org/3/movie/693134?api_key=0f7ff543b9146c27bb69c85b227e5f63&append_to_response=credits,reviews").to_return(status: 200, body: json_response)
 
     # As a user, when I visit a movie's detail page (`/users/:user_id/movies/:movie_id`) where :id is a valid user id,
     visit "/users/#{@user.id}/movies/693134"
 
-    save_and_open_page
-
-    # # I should see
-    # within ".links" do
-    #   # a button to Create a Viewing Party
-    #   expect(page).to_have_content("")
-    #   # a button to return to the Discover Page
-    # end
-    
-    # # I should also see the following information about the movie:
-    
-    # # - Movie Title
-    # # - Vote Average of the movie
-    # # - Runtime in hours & minutes
-    # # - Genre(s) associated to movie
-    # # - Summary description
-    # # - List the first 10 cast members (characters & actress/actors)
-    # # - Count of total reviews
-    # # - Each review's author and information
+    # I should see
+    within ".links" do
+      expect(page).to have_link("Create a Viewing Party")
+      expect(page).to have_link("Return to Discover")
     end
+    
+    # I should also see the following information about the movie:
+    within ".movie_details" do
+      expect(page).to have_content("Dune: Part Two")
+      expect(page).to have_content("8.215")
+      expect(page).to have_content("Runtime: 2 Hours and 47 Minutes") 
+    end
+
+    # Movie Summary
+    within ".movie_description" do
+      expect(page).to have_content("Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.")
+    end
+
+    # Movie Genre(s)
+    within ".movie_genre" do
+      expect(page).to have_content("Science Fiction")
+      expect(page).to have_content("Adventure")
+    end
+
+    # Movie Cast
+    within ".movie_cast" do
+      expect(page).to have_content("Timothée Chalamet")
+      expect(page).to have_content("Zendaya")
+      expect(page).to have_content("Rebecca Ferguson")
+    end
+
+    # Movie Reviews
+    within ".movie_reviews" do
+      expect(page).to have_content("Total Reviews: 9")
+      expect(page).to have_content("Review by: Manuel São Bento")
+        # Add Review - weird string thing
+      expect(page).to have_content("Review by: r96sk")
+        # Add Review - weird string thing
+    end
+  end
 end
