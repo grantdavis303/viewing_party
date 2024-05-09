7a0925002993c5eb1698e6c72bfc575def7899fb
Grant Davis <grantdavis303@gmail.com>
2024-05-09 07:54:18 -0600

    Update Controllers

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -10,16 +10,32 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @user = User.find(params[:id])
 
+    new_viewing_party = ViewingParty.create!(
+      duration: params[:party_duration],
+      date: params[:party_date],
+      start_time: "#{params["game"]["game_time(4i)"].to_i}:#{params["game"]["game_time(5i)"].to_i}",
+    )
+
+    new_user_party = UserParty.create!(
+      viewing_party: new_viewing_party.id, 
+      user: @user.id, 
+      host: true
+    )
+    #binding.pry
+    redirect_to "/"
     #<ActionController::Parameters {"authenticity_token"=>"6dwMEU9NrPfuQKeZDlDMpgXXR3H51rf8mN5Cz0c0xAx3IW4N3JRdWmjmstXyh6orzAYAAZwZ0UgwJXDPwmNG2g", "party_duration"=>"126", "party_date"=>"2024-05-30", "game"=>{"game_time(4i)"=>"13", "game_time(5i)"=>"15"}, "guest_1"=>"freddy@sanford-balistreri.test", "commit"=>"Create Party", "controller"=>"viewing_parties", "action"=>"create", "id"=>"10", "movie_id"=>"746036"} permitted: false>
-
     # ViewingParty.create!(
     #   duration: params[:party_duration], 
     #   date: params[:party_date], 
     #   start_time: "#{params["game"]["game_time(4i)"].to_i}:#{params["game"]["game_time(5i)"].to_i}"
     # )
-
     # redirect_to "/users/#{params[:id]}"
+  end
+
+  def show
+    binding.pry
+    @party = ViewingParty.find("")
   end
 end
